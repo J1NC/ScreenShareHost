ac70ccae56d978203329340b02fac2d820871750
J1NC <womin1120@naver.com>
2019-06-03 18:17:41 +0900

    [ADD] 컴퓨터 화면을 비트맵으로 변환

--- ScreenShareHost/Form1.Designer.cs
+++ ScreenShareHost/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ScreenShareHost
 {
-    partial class Form1
+    partial class ScreenShare
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -29,12 +29,55 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pbScreen = new System.Windows.Forms.PictureBox();
+            this.btnStreaming = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbScreen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbScreen
+            // 
+            this.pbScreen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbScreen.Location = new System.Drawing.Point(267, 12);
+            this.pbScreen.Name = "pbScreen";
+            this.pbScreen.Size = new System.Drawing.Size(985, 657);
+            this.pbScreen.TabIndex = 0;
+            this.pbScreen.TabStop = false;
+            // 
+            // btnStreaming
+            // 
+            this.btnStreaming.Location = new System.Drawing.Point(110, 621);
+            this.btnStreaming.Name = "btnStreaming";
+            this.btnStreaming.Size = new System.Drawing.Size(151, 48);
+            this.btnStreaming.TabIndex = 1;
+            this.btnStreaming.Text = "Streaming";
+            this.btnStreaming.UseVisualStyleBackColor = true;
+            this.btnStreaming.Click += new System.EventHandler(this.btnStreaming_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ScreenShare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.btnStreaming);
+            this.Controls.Add(this.pbScreen);
+            this.Name = "ScreenShare";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbScreen)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbScreen;
+        private System.Windows.Forms.Button btnStreaming;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- ScreenShareHost/Form1.cs
+++ ScreenShareHost/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,53 @@ using System.Windows.Forms;
 
 namespace ScreenShareHost
 {
-    public partial class Form1 : Form
+    public partial class ScreenShare : Form
     {
-        public Form1()
+        private const int GCCollectCycle = 20;
+        private int Cycle = 0;
+        public ScreenShare()
         {
             InitializeComponent();
+        }
+
+        public Bitmap getScreenImage()
+        {
+            Size size = new Size(pbScreen.Width, pbScreen.Height);
+            Bitmap screenWithCursor = ScreenCapture.CapturePrimaryScreen(true);
+            if (screenWithCursor != null)
+                return new Bitmap(screenWithCursor, size);
+            else
+                return null;
+        }
+
+        private void btnStreaming_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == false)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            {
+                pbScreen.Image = null;
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Bitmap BM = getScreenImage();
+            if (pbScreen.Image != null)
+            {
+                pbScreen.Image.Dispose();
+            }
+            if (BM != null)
+            {
+                pbScreen.Image = BM;
+            }
+            Debug.WriteLine(Cycle);
+            Cycle++;
+            if (Cycle % GCCollectCycle == 0)
+                GC.Collect();
         }
     }
 }
